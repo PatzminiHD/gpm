97c6c82aa2fd8b1173b549c92fa83c289de0f559
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-08-15 00:29:28 +0200

    Add AppSettings

---
+++ gpm/AppSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatzminiHD.CSLib.Settings;
+
+namespace gpm
+{
+    public class AppSettings : Base
+    {
+        public class UpdateSettings
+        {
+            public List<(string name, string githubRepoOwner, string githubRepo, string localDirectoryPath)>? updateApplications;
+            public string? versionTrackerFileName;
+            public string? tagVersionSeperator;
+            public string? fileVersionSeperator;
+        }
+
+        public UpdateSettings updateSettings;
+
+        public AppSettings()
+        {
+            ApplicationName = "gpm";
+            ApplicationVersion = "v0.1.0";
+
+            updateSettings = new UpdateSettings()
+            {
+                updateApplications = new()
+                {
+                    ("PatzminiHD.CSLib", "PatzminiHD", "PatzminiHD.CSLib", "E:\\Projects\\Programming\\CSharp\\gpm\\gpm\\lib\\")
+                },
+                versionTrackerFileName = "CurrentVersion.txt",
+                tagVersionSeperator = "/",
+                fileVersionSeperator= "_",
+            };
+        }
+    }
+}
